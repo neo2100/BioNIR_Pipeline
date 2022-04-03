15c07cc499278624565fc03a2242d3c9db8175ab
Hadi <65487316+neo2100@users.noreply.github.com>
2022-04-03 14:33:59 +0200

    setup requirements added

--- setup.py
+++ setup.py
@@ -11,6 +11,12 @@ with open('README.md') as f:
 with open('LICENSE') as f:
     license = f.read()
 
+REQUIRED = [
+    '-e git+git://github.com/iacopy/pymed.git@436b5af0892ec8cd101c5dcd1d54c7a51f73a145#egg=pymed',
+    'nltk>=3.6.7',
+    'sentence-transformers>=2.1.0'
+]
+
 setup(
     name='bionir_pipeline',
     version='0.1.0',
@@ -20,5 +26,6 @@ setup(
     author_email='mohammad_sh2100@yahoo.com',
     url='https://github.com/neo2100/BioNIR_Pipeline',
     license=license,
-    packages=find_packages(exclude=('tests', 'examples', 'docs'))
+    packages=find_packages(exclude=('tests', 'examples', 'docs')),
+    install_requires=REQUIRED
 )
