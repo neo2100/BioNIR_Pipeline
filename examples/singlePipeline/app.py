01ff2a6b4d8e3fde0c5207e69e5f624f25d27caa
Hadi <65487316+neo2100@users.noreply.github.com>
2022-04-05 14:53:13 +0200

    coreNLP added

--- examples/singlePipeline/app.py
+++ examples/singlePipeline/app.py
@@ -10,9 +10,11 @@ pipeline = Pipeline()
 #pipeline.push("PubMedSimpleSearch as documentRetrieval", {'maxDocumentNumber': 2})
 pipeline.push("PubMedAdvancedSearch as documentRetrieval", {'maxAroundDocumentNumber': 500,'fetchMaxDocumentNumber':1500})
 pipeline.push("MiddleBM25 as documentRetrieval", {'maxDocumentNumber': 10})
+pipeline.push("SentenceSplittingByNLTK as preprocessing", {'outputName': 'originalSentences'})
+pipeline.push("CoreNLPAnnotator as preprocessing", {'properties':{'annotators': 'tokenize, ssplit, pos, lemma, ner, parse, coref', 'coref.algorithm': 'neural', 'coref.neural.greedyness': '0.51'}})
 pipeline.push("CoreferenceResolverByKGen as preprocessing", {})
 pipeline.push("AbbreviationResolverByKGen as preprocessing", {})
-pipeline.push("SentenceSplittingByNLTK as preprocessing", {})
+pipeline.push("SentenceSplittingByNLTK as preprocessing", {'outputName': 'sentences'})
 pipeline.push("SBERT as embedding", {
               'modelName': "sentence-transformers/multi-qa-mpnet-base-cos-v1"})
 pipeline.push("VectorSimilarity as ranking", {
